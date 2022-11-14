e2a6d899df7d8e26cd5e1f5a965313b1dbf318e3
Peter Denno <podenno@gmail.com>
2022-11-13 19:16:43 -0500

    New RADmapper SHA

--- src/rm_exerciser/app/core.cljs
+++ src/rm_exerciser/app/core.cljs
@@ -16,79 +16,98 @@
 
 ;;; ToDo: Comments were messing up parse?!?
 (def init-text
-"(  $DBa := [{'id' : 123, 'aAttr' : 'Bob-A-data',   'name' : 'Bob'},
-            {'id' : 234, 'aAttr' : 'Alice-A-data', 'name' : 'Alice'}];
+"(  $DBa := [{'email' : 'bob@example.com', 'aAttr' : 'Bob-A-data',   'name' : 'Bob'},
+             {'email' : 'alice@alice.org', 'aAttr' : 'Alice-A-data', 'name' : 'Alice'}];
 
-   $DBb := [{'id' : 123, 'bAttr' : 'Bob-B-data'},
-            {'id' : 234, 'bAttr' : 'Alice-B-data'}];
+   $DBb := [{'id' : 'bob@example.com', 'bAttr' : 'Bob-B-data'},
+            {'id' : 'alice@alice.org', 'bAttr' : 'Alice-B-data'}];
 
-   $qFn :=  query(){[$DBa ?e1 :id    ?id]
+   $qFn :=  query(){[$DBa ?e1 :email ?id]
                     [$DBb ?e2 :id    ?id]
                     [$DBa ?e1 :name  ?name]
                     [$DBa ?e1 :aAttr ?aData]
                     [$DBb ?e2 :bAttr ?bData]};
 
-   $bSets := $qFn($DBa, $DBb);
+   $bSet := $qFn($DBa, $DBb);
 
    $eFn := express(){{?id : {'name'  : ?name,
                              'aData' : ?aData,
                              'bData' : ?bData}}};
 
-   $reduce($bSets, $eFn) )")
+   $reduce($bSet, $eFn) )")
 
 ;;; ToDo: Some kind of :onResize (of the window)
 (defn home-page [_s]
   [:body
-   {:style {:width  #_"800px" (str (- (.-innerWidth js/window) 15)  "px")
-            :height #_"500px" (str (.-innerHeight js/window) "px")
+   {:style {:width  "auto" #_"100%" #_(str (- (.-innerWidth  js/window)  15) "px") ; https://ishadeed.com/article/auto-css/
+            :height "auto" #_"100%" #_(str (- (.-innerHeight js/window) 100) "px")
             :margin-left  "0px" :margin-right "0px"  :margin-top "0px"  :margin-bottom "0px" ; Margins/padding useless everywhere!
-            :padding-left "0px" :padding-right "0px" :padding-top "0px" :padding-bottom "0px"}}
+            :padding-left "0px" :padding-right "0px" :padding-top "0px" :padding-bottom "0px"
+            :border "3px dotted red"}}
    [:section.hero.is-primary.is-small ; ToDo: ...but not small enough!
     [:div.hero-body [:div.title.is-large "RADmapper Exerciser" ]]]
    [:container
-    {:style {:display "grid" :grid-gap "0px" :grid-gutter-width "0px"
+    {:componentDidMount #(println "mounting!:" %)
+     :onMount #(println "rmmmesizing container!:" %)
+     :onChange #(println "resizing container!:" %)
+     :style {:display "grid"
+             :grid-template-areas "\"data  editor\" \n \"data output\""
+             :grid-template-columns "auto-fit auto-fit" ; Does nothing
+             :grid-template-row     "auto-fit auto-fit"
+             ;;:grid-template-columns "repeat(auto-fit, minmax(200px,1fr) )" ; Cause overrun (as though the LHS isn't ther)
+             ;;:grid-template-row     "repeat(auto-fit, minmax(200px,1fr) )"
+             :grid-column-gap "0px"
+             :grid-row-gap "0px"
+             :grid-gutter-width  "0px" ; Does nothing.
+             :grid-gutter-height "0px" ; Does nothing.
              :overflow "hidden" ; This needed or whole-window horizontal follows the mouse.
-             #_#_:grid-template-columns "auto"
-             #_#_:grid-template-rows "auto"
-             :border "3px solid"
-             :margin-left  "0px" :margin-right "0px"  :margin-top "0px"  :margin-bottom "0px"
-             :padding-left "0px" :padding-right "0px" :padding-top "0px" :padding-bottom "0px"
-             #_#_:max-width "90%"}} ; No longer makes a difference. (and doesn't work)
-    [:textarea
-      {:defaultValue "/*   Use in-lined data for the time being!  */"
-       :style {:gird-row-start    1 :gird-row-end    3
-               :grid-column-start 1 :grid-column-end 2
-               :max-width "60%" ; Would like to do 90/10 here (with shrinks at 90%. Why?
-               :min-width "10%"
+             :height "100%"     ; This needed or whole-window vertical   follows the mouse. <============ STILL
+             :border "3px solid"}}
+    [:item
+     {:on-change #(println "resizing item!:" %)
+      :style {:grid-area "data"
+              :flex "1 1 auto"
+              :display "flex"
+              :resize "horizontal"
+              :justify-content "stretch"
+              :border "3px solid"
+              :overflow "auto"}}
+     [:textarea
+      {:defaultValue "/* FACTORED  Use in-lined data for the time being!  */"
+       :onChange #(println "resizing textarea!:" %)
+       :style {:width "auto" #_"100%"
+               :height "100%"
+               :flex "1 1 auto"
+               :object-fit "cover"
                :border "3px solid"
                :overflow "auto"
-               :resize "horizontal"
                :padding-left "5px"
                :padding-top "5px"
-               #_:width "100%"
-               #_:height "100%"}}]
+               :resize "auto"}}]]
     [editor init-text {:eval? true}]
-    [:textarea
-       {:class "textarea is-family-monospace editable monospace text-sm m-0"
-        :style {:grid-row-start    2 :grid-row-end    3 
-                :grid-column-start 2 :grid-column-end 3
-                :border "3px solid"
-                :height "100%"
-                :background-color "#EFF0EB" ; ToDo: Add through sass.
-                :overflow "auto"
-                ;;:max-width "100%" ; Does nothing
-                :max-height "90%" ; These match ...
-                :min-height "10%" ; the thing it is sharing with ................................WORKING!
-                :padding-left "5px"
-                :padding-top  "5px"
-                :resize "none"}
+    [:item
+     {:style {:grid-area "output"
+              :display "flex"
+              :flex "1 1 auto"
+              :justify-content "stretch"
+              :object-fit "cover"
+              :border "3px dotted green" :overflow "hidden" :resize "none"}}
+     [:textarea
+      {:style {:display "flex"
+               :height "100%"
+               :width "100%"
+               :background-color "#EFF0EB" ; ToDo: Add through sass.
+               :overflow "auto"
+               :padding-left "5px"
+               :padding-top  "5px"
+               :resize "none"}
         :onChange #(println "I'm having fun:" %) ; ToDo: Probably could be better ;^)
         :value (or (when-some [{:keys [error result]} @last-result]
                      (.log js/console (str "result = " (or result error)))
                      (if error
                        (str error)
                        (str result)))
-                   "Ctrl-Enter above to execute.")}]]])
+                   "Ctrl-Enter above to execute.")}]]]])
 
 (def app-state
   (r/atom {:rand (rand)}))
@@ -104,8 +123,7 @@
   (.theme EditorView
           (j/lit {".cm-content" {:white-space "pre-wrap"
                                  :padding "10px 0"
-                                 ;; :flex makes it a flex box and no scroll horizontal scroll bar.
-                                 #_#_ :flex "1 1 0"}
+                                 :flex "1 1 0"}
                   "&.cm-focused" {:outline "0 !important"}
                   ".cm-line" {:padding "0 9px"
                               :line-height "1.3"
@@ -146,19 +164,19 @@
                                                      source)
                                                     :parent el)))))]
     [:item
-     {:ref mount!
-      :style {:grid-row-start    1 :grid-row-end    2
-              :grid-column-start 2 :grid-column-end 3
-              :overflow "auto"
+     {:style {:grid-area "editor"
+              :display "flex"
+              :flex "1 1 auto" ; Or "1 1 0"  https://ishadeed.com/article/auto-css/
+              :width "100%"
+              :height "100%"
+              :flex-basis "auto"
+              :justify-content "stretch"
               :resize "vertical"
-              :max-height "90%" ; This is keep the output textarea from disappearing...
-              :min-height "10%" ; and match the thing it is sharing with.................. WORKING!
-              :max-width "100%"
-              :margin-left  "0px" :margin-right "0px"  :margin-top "0px"  :margin-bottom "0px"
-              :padding-left "0px" :padding-right "0px" :padding-top "0px" :padding-bottom "0px"
+              :overflow "auto"
+              :object-fit "cover"
               :border "3px solid"
-              :scroll-x "true"
-              :lineWrapping "false"}}]
+              :scroll-x "true" }}
+     [:div {:ref mount! :style {:display "flex" :object-fit "cover" :lineWrapping "false" :margin "auto"}}]]
       (finally
         (println "In finally")
         (j/call @!view :destroy))))
